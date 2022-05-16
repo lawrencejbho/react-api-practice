e6025f2977b84ec12c24da26c22c110bdc2d7a63
Lawrence Ho <lawrence.jb.ho@gmail.com>
2022-05-15 19:27:38 -0700

    added in saves in windowtracker.js

--- src/WindowTracker.js
+++ src/WindowTracker.js
@@ -4,13 +4,17 @@ function WindowTracker() {
 
     const [width, setWidth] = useState(window.innerWidth)
 
+// useEffect to allow us to add a window event listener 
     useEffect(() => {
+
+        // we create this function first so that we can use it for adding the event listener and cleanup
         function watchWidth() {
             setWidth(window.innerWidth)
         }
-    
+    // add the event listener
     window.addEventListener("resize", watchWidth)
 
+    // cleanup function, every time the windowtracker gets removed it will run this 
     return function() {
         window.removeEventListener("resize", watchWidth)
     }

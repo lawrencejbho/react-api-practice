f9922a778846dbe028d15d33bc1ee194f82b959b
Lawrence Ho <lawrence.jb.ho@gmail.com>
2022-05-15 18:05:48 -0700

    added use effect to update on button push

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 
 
 
 function App() {
 
-  const [starWarsData, setStarWarsData] = React.useState({})
+  const [starWarsData, setStarWarsData] = useState({})
+  const [count, setCount] = useState(1)
 
-  fetch("https://swapi.dev/api/people/1")
+  useEffect(() => {
+    console.log(starWarsData)
+    fetch(`https://swapi.dev/api/people/${count}`)
         .then(res => res.json())
         .then(data => setStarWarsData(data))
+      }, [count]);
+
+// uses a dependency array so that it will only run when count is modified
+
+    
+
 
   return (
     <div>
       <pre>{JSON.stringify(starWarsData,null,2)}</pre>
+      <button onClick={() => setCount(prevValue => (prevValue + 1))}>Get New Character</button>
     </div>
   );
 }
